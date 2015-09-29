c52374dcc7356027de43340d33ac3827f4078a88
Efeito Joule Oficial <efeitojouleoficial@gmail.com>
2015-09-29 13:02:38 -0300

    Revert "Páginas para aluno"
    This reverts commit 8aae05f3f4255d2cd2d9d29d8021b923e24c79f7.
    
    Conflicts:
    	Efeito Joule/src/br/com/joule/bean/UsuarioBean.java
    	Efeito Joule/src/br/com/joule/entity/Historico.java

--- Efeito Joule/src/br/com/joule/dao/HistoricoDAO.java
+++
@@ -1,9 +0,0 @@
-package br.com.joule.dao;
-
-import br.com.joule.entity.Historico;
-
-public interface HistoricoDAO extends DAO<Historico, Long> {
-
-	Historico buscarPorAulaAluno(int aula, int aluno);
-
-}
--- Efeito Joule/src/br/com/joule/dao/UsuarioDAO.java
+++
@@ -1,7 +0,0 @@
-package br.com.joule.dao;
-
-import br.com.joule.entity.Aluno;
-
-public interface UsuarioDAO extends DAO<Aluno, Integer> {
-
-}
